9654779f2000ee0f536989310351c62a4954ffad
coreyo-git <corey.oconnor@live.com.au>
2025-07-11 08:58:55 +1000

    godotenv library added, reading of env file working

--- src/main.go
+++ src/main.go
@@ -1,2 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+	"fmt"
+
+	// "github.com/bmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("TOKEN")
+
+	fmt.Println("Token:", token)
+	// sess, err := discordgo.New("Bot " + token)
+}
