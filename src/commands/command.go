e286ddea8121867c13895ee5ad3d3942b588c523
coreyo-git <corey.oconnor@live.com.au>
2025-07-11 16:07:09 +1000

    feat: commands, modular slash command handler command-centric architecture for handling slash commands. Each command is now a self-contained unit, defining its own Discord application command data and its corresponding handler function.
    - Adds a generic Command struct in src/commands/command.go to define the structure for all slash commands.
    - Implements the first command, /play, in src/commands/play.go.
    - The /play command accepts a required query option and delegating the logic to handleURL() and handleSearch().

---
+++ src/commands/command.go
@@ -0,0 +1,14 @@
+package commands
+
+import "github.com/bwmarrin/discordgo"
+
+// A map of all the registered commands, keyed by command name.
+var Commands = make(map[string]Command)
+
+// Command holds the definition and handler for a slash commands.
+type Command struct {
+	// Data sent to Discord to register the command.
+	Definition *discordgo.ApplicationCommand
+	// Function that runs when the command is used.
+	Handler func(s *discordgo.Session, i *discordgo.InteractionCreate)
+}
---
+++ src/commands/play.go
@@ -0,0 +1,93 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"net/url"
+)
+
+func playHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	var query string
+
+	// Access options in the order provided by the user.
+	options := i.ApplicationCommandData().Options
+
+	// convert the slice into a map
+	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+	for _, opt := range options {
+		optionMap[opt.Name] = opt
+	}
+
+	query = optionMap["query"].StringValue()
+
+
+	// Acknowledge command and reply to avoid timeout.
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("Received your request for `%s`!", query),
+		},
+	})
+
+	if err != nil {
+		s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
+			Content: "Something went wrong while trying to respond.",
+		})
+		log.Printf("Error responding to interaction: %v", err)
+	}
+
+	if isValidURL(query) {
+		handleURL(s, i, query)
+	} else {
+		handleSearch(s, i, query)
+	}
+}
+
+func init() {
+	minLength := 3
+	Commands["play"] = Command{
+		Definition: &discordgo.ApplicationCommand{
+			Name:        "play",
+			Description: "Plays a song from a URL",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "query",
+					Description: "The URL of the song or a search term.",
+					Required:    true,
+					MinLength:   &minLength,
+				},
+			},
+		},
+		Handler: playHandler,
+	}
+}
+
+// Called when the user's query is a URL.
+func handleURL(s *discordgo.Session, i *discordgo.InteractionCreate, query string) {
+	_, err := s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
+		Content: fmt.Sprintf("Getting song from: `%s`", query),
+	})
+
+	if err != nil {
+		log.Panicf("Error during handleURL: %v", err)
+	}
+}
+
+// called when the user's query is a song name
+func handleSearch(s *discordgo.Session, i *discordgo.InteractionCreate, query string) {
+	_, err := s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
+		Content: fmt.Sprintf("Searching for: `%s`", query),
+	})
+
+	if err != nil {
+		log.Panicf("Error during handleSearch: %v", err)
+	}
+}
+
+// checks if a string is a valid URL.
+func isValidURL(s string) bool {
+	_, err := url.ParseRequestURI(s)
+	return err == nil
+}
