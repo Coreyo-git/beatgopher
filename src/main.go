4c7c992ead896b4636af493819f2790185546337
coreyo-git <corey.oconnor@live.com.au>
2025-07-11 10:10:24 +1000

    Config, session intents, message handler and session websocket init

--- src/main.go
+++ src/main.go
@@ -1,23 +1,66 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"fmt"
+	"os/signal"
+	"syscall"
 
-	// "github.com/bmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load .env file
+	// Load the .env file to get the bot token.
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Get the bot token from the environment variables.
 	token := os.Getenv("TOKEN")
 
-	fmt.Println("Token:", token)
-	// sess, err := discordgo.New("Bot " + token)
+	// Create a new Discord session using the provided bot token.
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+
+	// Add a handler for the messageCreate event.
+	// This function will be called every time a new message is created on any channel that the authenticated user has access to.
+	// TODO: Give it a configurable channel scope or maybe just give it permission for one music channel??
+	session.AddHandler(messageCreate)
+
+	// messages and voice states.
+	// https://discord.com/developers/docs/topics/gateway#gateway-intents
+	session.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates
+
+	// Open a websocket connection to Discord.
+	err = session.Open()
+	if err != nil {
+		log.Fatalf("Error opening connection: %v", err)
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	session.Close()
+}
+
+// messageCreate will be called every time a new message is created.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Bast test setup response
+	if m.Content == "hello" {
+		s.ChannelMessageSend(m.ChannelID, "world!")
+	}
 }
